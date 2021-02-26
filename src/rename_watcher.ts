038561c0264e35cf0ae5f518009f26f1070d02cd
Andreas Blech <andreas.blech@t-online.de>
2021-02-26 08:48:58 +0100

    rename-watcher: path to package is changed on rename/move operation

--- src/rename_watcher.ts
+++ src/rename_watcher.ts
@@ -11,29 +11,45 @@ export function activate() {
     event.files.forEach(async (fileChange)  => {
 
         //XXX: Multiple files in folder handeln mit nested foldern
-        //XXX: Change Path to sourcefile in test file (path to package)
 
 
         if(fileOperations.isPathInLibFolder(fileChange.oldUri.path)) {
 
           var oldtestFilePath = fileOperations.getPathOfTestFile(fileChange.oldUri.path);
-          console.log(oldtestFilePath);
+          //console.log(oldtestFilePath);
 
           var testFileName = fileOperations.getNameOfTestFile(fileChange.oldUri.path);
 
           if(fs.existsSync(oldtestFilePath)) {
-            var selectedItem = await vscode.window.showInformationMessage("Do you want to move " + testFileName, "Yes, move", "No");
-            if(selectedItem === "Yes, move") {
+            var selectedItem = await vscode.window.showInformationMessage("Do you want to move/rename " + testFileName + "?", "Yes", "No");
+            if(selectedItem === "Yes") {
               
               var newTestFilePath = fileOperations.getPathOfTestFile(fileChange.newUri.path);
               
               //Verzeichnisse die nicht existieren erstellen
               fs.mkdir(path.dirname(newTestFilePath), { recursive: true }, (err) => {
 
-                fs.copyFileSync(oldtestFilePath, newTestFilePath);
 
-                fs.unlinkSync(oldtestFilePath);
+                //XXX: VSCode erkennt nicht, dass die Datei verschoben wurde
+                //-> Müssen nach aktive editors schauen und das modifizieren
+                fs.renameSync(oldtestFilePath,newTestFilePath)
+
                 //XXX: Nun leere Ordner, löschen
+
+                //Dies bezieht sich nicht auf die Open als Registerkarte sichtbaren editoren
+                //var editors = vscode.workspace.;
+
+
+                //Change Path to sourcefile in test file (path to package)
+                var relativePathOld = fileOperations.getRelativePathInLibFolder(fileChange.oldUri.path);
+                var relativePathNew = fileOperations.getRelativePathInLibFolder(fileChange.newUri.path);
+
+                console.log("Change \n" + relativePathOld + "\n to \n" + relativePathNew);
+                var content = fs.readFileSync(newTestFilePath).toString();
+                var content = content.replace(relativePathOld, relativePathNew);
+                fs.writeFileSync(newTestFilePath, content);
+
+
 
                 vscode.window.showInformationMessage("Moved file to " + newTestFilePath);
               });
